b1fad0a315c0a7b9fff067221ff6b22f66628cac
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-10 12:24:41 +0430

    add Timer to onCreate function in app class for show log

--- app/src/main/java/ir/ah/app/runnerman/App.kt
+++ app/src/main/java/ir/ah/app/runnerman/App.kt
@@ -2,7 +2,12 @@ package ir.ah.app.runnerman
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class App:Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
 }
