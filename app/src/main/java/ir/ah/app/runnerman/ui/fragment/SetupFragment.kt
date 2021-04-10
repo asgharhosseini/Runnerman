e773413fb395a4cd277d0ab5f3aee096868679d8
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-10 15:20:40 +0430

    and onClick for navigate to TrackingFragment.kt

--- app/src/main/java/ir/ah/app/runnerman/ui/fragment/SetupFragment.kt
+++ app/src/main/java/ir/ah/app/runnerman/ui/fragment/SetupFragment.kt
@@ -1,8 +1,19 @@
 package ir.ah.app.runnerman.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ir.ah.app.runnerman.R
+import kotlinx.android.synthetic.main.fragment_setup.*
+
 @AndroidEntryPoint
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
 }
