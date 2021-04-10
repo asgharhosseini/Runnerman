14381e2c5cd72503c401134201bec0c1e291af9a
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-10 12:54:41 +0430

    add Annotation AndroidEntryPoint

--- app/src/main/java/ir/ah/app/runnerman/ui/fragment/SettingsFragment.kt
+++ app/src/main/java/ir/ah/app/runnerman/ui/fragment/SettingsFragment.kt
@@ -1,7 +1,8 @@
 package ir.ah.app.runnerman.ui.fragment
 
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import ir.ah.app.runnerman.R
-
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 }
--- app/src/main/java/ir/ah/app/runnerman/ui/fragment/SetupFragment.kt
+++ app/src/main/java/ir/ah/app/runnerman/ui/fragment/SetupFragment.kt
@@ -1,7 +1,8 @@
 package ir.ah.app.runnerman.ui.fragment
 
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import ir.ah.app.runnerman.R
-
+@AndroidEntryPoint
 class SetupFragment : Fragment(R.layout.fragment_setup) {
 }
