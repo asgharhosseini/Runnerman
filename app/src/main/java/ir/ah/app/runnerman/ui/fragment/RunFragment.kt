dcf581a6ef8dd1cadcbbe7c7f55986e658a9ed8b
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-10 12:54:07 +0430

    add fab setting for navigate

--- app/src/main/java/ir/ah/app/runnerman/ui/fragment/RunFragment.kt
+++ app/src/main/java/ir/ah/app/runnerman/ui/fragment/RunFragment.kt
@@ -1,13 +1,23 @@
 package ir.ah.app.runnerman.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ir.ah.app.runnerman.R
 import ir.ah.app.runnerman.ui.viewmodel.MainViewModel
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        fab.setOnClickListener {
+            findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
+        }
+    }
 }
