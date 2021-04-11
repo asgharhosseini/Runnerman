1fcf45ef3354b02a046705de75874911c73f52d2
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-11 10:46:11 +0430

    get action in notification and navigate to fragment

--- app/src/main/java/ir/ah/app/runnerman/ui/MainActivity.kt
+++ app/src/main/java/ir/ah/app/runnerman/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package ir.ah.app.runnerman.ui
 
+import android.content.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ir.ah.app.runnerman.R
+import ir.ah.app.runnerman.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +17,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigateToTrackingFragmentIfNeeded(intent)
+
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
         navHostFragment.findNavController()
@@ -25,5 +29,16 @@ class MainActivity : AppCompatActivity() {
                     else -> bottomNavigationView.visibility = View.GONE
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if (intent?.action==ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
