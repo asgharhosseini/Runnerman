a169673618c83a912943843326e3e369b3848f75
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-12 12:40:48 +0430

    add menu for stop service in top menu in fragment

--- app/src/main/java/ir/ah/app/runnerman/ui/fragment/TrackingFragment.kt
+++ app/src/main/java/ir/ah/app/runnerman/ui/fragment/TrackingFragment.kt
@@ -1,26 +1,26 @@
 package ir.ah.app.runnerman.ui.fragment
 
-import android.content.Intent
-import android.os.Bundle
-import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import android.content.*
+import android.os.*
+import android.view.*
+import androidx.fragment.app.*
+import androidx.lifecycle.*
+import androidx.navigation.fragment.*
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.*
+import com.google.android.material.dialog.*
+import dagger.hilt.android.*
 import ir.ah.app.runnerman.R
+import ir.ah.app.runnerman.other.*
 import ir.ah.app.runnerman.other.Constants.ACTION_PAUSE_SERVICE
 import ir.ah.app.runnerman.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import ir.ah.app.runnerman.other.Constants.ACTION_STOP_SERVICE
 import ir.ah.app.runnerman.other.Constants.MAP_ZOOM
 import ir.ah.app.runnerman.other.Constants.POLYLINE_COLOR
 import ir.ah.app.runnerman.other.Constants.POLYLINE_WIDTH
+import ir.ah.app.runnerman.service.*
 import ir.ah.app.runnerman.service.Polyline
-import ir.ah.app.runnerman.service.Polylines
-import ir.ah.app.runnerman.service.TrackingService
-import ir.ah.app.runnerman.ui.viewmodel.MainViewModel
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.PolylineOptions
-import dagger.hilt.android.AndroidEntryPoint
-import ir.ah.app.runnerman.other.*
+import ir.ah.app.runnerman.ui.viewmodel.*
 import kotlinx.android.synthetic.main.fragment_tracking.*
 
 @AndroidEntryPoint
@@ -34,6 +34,17 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private var map: GoogleMap? = null
 
     private var curTimeInMillis = 0L
+
+    private var menu: Menu? = null
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -61,18 +72,61 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         })
 
         TrackingService.timeRunInMillis.observe(viewLifecycleOwner, Observer {
-            curTimeInMillis=it
-            val formattedTime=TrackingUtility.getFormattedStopWatchTime(curTimeInMillis,true)
+            curTimeInMillis = it
+            val formattedTime = TrackingUtility.getFormattedStopWatchTime(curTimeInMillis, true)
             tvTimer.text = formattedTime
         })
     }
 
     private fun toggleRun() {
         if(isTracking) {
+            menu?.getItem(0)?.isVisible = true
             sendCommandToService(ACTION_PAUSE_SERVICE)
         } else {
             sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.toolbar_tracking_menu, menu)
+        this.menu = menu
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        if(curTimeInMillis > 0L) {
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miCancelTracking -> {
+                showCancelTrackingDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCancelTrackingDialog() {
+        val dialog = MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme)
+                .setTitle("Cancel the Run?")
+                .setMessage("Are you sure to cancel the current run and delete all its data?")
+                .setIcon(R.drawable.ic_delete)
+                .setPositiveButton("Yes") { _, _ ->
+                    stopRun()
+                }
+                .setNegativeButton("No") { dialogInterface, _ ->
+                    dialogInterface.cancel()
+                }
+                .create()
+        dialog.show()
+    }
+
+    private fun stopRun() {
+        sendCommandToService(ACTION_STOP_SERVICE)
+        findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
     }
 
     private fun updateTracking(isTracking: Boolean) {
@@ -82,6 +136,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
             btnFinishRun.visibility = View.VISIBLE
         } else {
             btnToggleRun.text = "Stop"
+            menu?.getItem(0)?.isVisible = true
             btnFinishRun.visibility = View.GONE
         }
     }
