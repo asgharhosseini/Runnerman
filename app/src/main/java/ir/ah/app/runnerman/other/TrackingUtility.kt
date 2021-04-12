3d4a83d46ee02812d99223e5f0295c536b7435e6
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-12 17:53:42 +0430

    add function for  calculate Polyline Length

--- app/src/main/java/ir/ah/app/runnerman/other/TrackingUtility.kt
+++ app/src/main/java/ir/ah/app/runnerman/other/TrackingUtility.kt
@@ -2,7 +2,9 @@ package ir.ah.app.runnerman.other
 
 import android.Manifest
 import android.content.Context
+import android.location.*
 import android.os.Build
+import ir.ah.app.runnerman.service.*
 import pub.devrel.easypermissions.EasyPermissions
 import java.util.concurrent.*
 
@@ -40,5 +42,23 @@ object TrackingUtility {
                 "${if(minutes < 10) "0" else ""}$minutes:" +
                 "${if(seconds < 10) "0" else ""}$seconds:" +
                 "${if(milliseconds < 10) "0" else ""}$milliseconds"
+    }
+    fun calculatePolylineLength(polyline: Polyline): Float {
+        var distance = 0f
+        for(i in 0..polyline.size - 2) {
+            val pos1 = polyline[i]
+            val pos2 = polyline[i + 1]
+
+            val result = FloatArray(1)
+            Location.distanceBetween(
+                    pos1.latitude,
+                    pos1.longitude,
+                    pos2.latitude,
+                    pos2.longitude,
+                    result
+            )
+            distance += result[0]
+        }
+        return distance
     }
 }
