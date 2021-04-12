5b0b8c0690e8f70205a6eb0b8a0bb8a32c1f5de2
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-12 17:52:45 +0430

    change module scope

--- app/src/main/java/ir/ah/app/runnerman/di/AppModule.kt
+++ app/src/main/java/ir/ah/app/runnerman/di/AppModule.kt
@@ -5,14 +5,15 @@ import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.components.SingletonComponent
 import ir.ah.app.runnerman.data.database.RunnerManDatabase
 import ir.ah.app.runnerman.other.Constants.RUNNING_DATABASE_NAME
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ApplicationComponent::class)
 object AppModule {
 
 
