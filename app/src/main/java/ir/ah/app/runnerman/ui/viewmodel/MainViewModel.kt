473a2d4bca425215c66be0f61eee5bb5b0b784f2
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-12 17:54:18 +0430

    add function for insert run to database

--- app/src/main/java/ir/ah/app/runnerman/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/ir/ah/app/runnerman/ui/viewmodel/MainViewModel.kt
@@ -1,10 +1,16 @@
 package ir.ah.app.runnerman.ui.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.*
+import ir.ah.app.runnerman.data.model.*
 import ir.ah.app.runnerman.data.repositories.MainRepository
+import kotlinx.coroutines.*
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+    fun insertRun(run:Run)=viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
