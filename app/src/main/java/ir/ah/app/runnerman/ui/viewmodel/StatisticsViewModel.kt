9890a2ac49a16b4a0ffe181346362ff5480365f2
asghar hosseini <asgharhosseini.v@gmail.com>
2021-04-14 12:19:32 +0430

    get Statistics data form database

--- app/src/main/java/ir/ah/app/runnerman/ui/viewmodel/StatisticsViewModel.kt
+++ app/src/main/java/ir/ah/app/runnerman/ui/viewmodel/StatisticsViewModel.kt
@@ -7,4 +7,11 @@ import ir.ah.app.runnerman.data.repositories.MainRepository
 class StatisticsViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+    val totalTimeRun = mainRepository.getTotalTimeInMillis()
+    val totalDistance = mainRepository.getTotalDistance()
+    val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+    val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+
 }
